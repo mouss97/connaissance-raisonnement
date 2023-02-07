488f5dd80ff8ab155d4d69fe01bf0d7ba6b646ce
moustapha LO <70775509+mouss97@users.noreply.github.com>
2023-02-07 20:07:05 +0100

    shuffle questions, add top scorer...

--- get_data.py
+++ get_data.py
@@ -4,7 +4,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 sparql.setQuery("""
-SELECT ?item ?itemLabel ?country ?countryLabel ?participants ?participantsLabel ?winner ?winnerLabel
+SELECT ?item ?itemLabel ?country ?countryLabel ?participants ?participantsLabel ?winner ?winnerLabel ?leader ?leaderLabel
 
 WHERE {
   ?item wdt:P3450 wd:Q19317;
@@ -13,17 +13,47 @@ WHERE {
       wdt:P1132 ?participants.
   ?item wdt:P3450 wd:Q19317;
       wdt:P1346 ?winner.
+  ?item wdt:P3450 wd:Q19317;
+      wdt:P3279 ?leader.
+
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 """)
+
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
 results_df = pd.io.json.json_normalize(results['results']['bindings'])
 results_df= results_df[['item.value', 'itemLabel.value', 'winnerLabel.value',\
-            'participantsLabel.value', 'countryLabel.value']].sort_values(by = "itemLabel.value")
+            'participantsLabel.value', 'countryLabel.value', 'leaderLabel.value']].sort_values(by = "itemLabel.value")
 
-results_df.columns = ['item', 'itemLabel', 'winnerLabel', 'participantsLabel', 'countryLabel']
+results_df.columns = ['item', 'itemLabel', 'winnerLabel', 'participantsLabel', 'countryLabel', 'leaderLabel']
+results_df = results_df.reset_index(drop=True)
+
+# Change the teams name to keep the country name only
+results_df['winnerLabel'] = [x.split(' ')[0] for x in results_df['winnerLabel']]
+
+# 2002 in both South Korea and Japan
+results_df = results_df[results_df['itemLabel'] != '2002 FIFA World Cup']
+results_df = results_df.append({'item': 'http://www.wikidata.org/entity/Q19317', 'itemLabel': '2002 FIFA World Cup',
+'winnerLabel': 'Brazil', 'participantsLabel': '32',
+'countryLabel': 'South Korea & Japan',
+'leaderLabel': 'José Luis Chilavert'}, ignore_index=True)
+
+# leaderLabel : cast to string
+results_df['leaderLabel'] = results_df['leaderLabel'].astype(str)
+# concatenate the leaders for each year
+results_df = results_df.groupby(
+    ['item', 'itemLabel', 'winnerLabel', 'participantsLabel', 'countryLabel']
+        ).agg({'leaderLabel': ', '.join}).reset_index()
+# keep an only leader for each year
+results_df.loc[results_df['itemLabel'] == '1962 FIFA World Cup', 'leaderLabel'] = 'Garrincha'
+results_df.loc[results_df['itemLabel'] == '1994 FIFA World Cup', 'leaderLabel'] = 'Oleg Salenko'
+results_df.loc[results_df['itemLabel'] == '2010 FIFA World Cup', 'leaderLabel'] = 'Thomas Müller'
+results_df.loc[results_df['itemLabel'] == '2022 FIFA World Cup', 'leaderLabel'] = 'Kylian Mbappé'
+
+# reset the index correctly
+results_df.sort_values(by = "itemLabel", inplace = True)
 results_df = results_df.reset_index(drop=True)
 #save
 results_df.to_csv('data.csv', index=False)
--- gui.py
+++ gui.py
@@ -29,29 +29,42 @@ class Quiz:
         self.frame_content = ttk.Frame(self.parent)
         self.frame_content.pack()
 
-        self.logo = PhotoImage(file = "logo.png")
-        ttk.Label(self.frame_header, image = self.logo).grid(row = 0, column = 0, rowspan = 2)
+        #self.logo = PhotoImage(file = "logo.png")
+        #ttk.Label(self.frame_header, image = self.logo).grid(row = 0, column = 0, rowspan = 2)
         ttk.Label(self.frame_header, text = "Quiz", style = "Header.TLabel").grid(row = 0, column = 1)
-        ttk.Label(self.frame_header, wraplength = 300,
-                text = ("Choose the right option from the list to answer the question. "
-                        "You can select only one option at a time.")).grid(row = 1, column = 1)
+        ttk.Label(self.frame_header, text = "").grid(row = 1, column = 2)
+
         self.bank = self.quiz_bank()
-        self.questions = self.bank['1930 FIFA World Cup']['Question']
+        self.questions = []
+        self.correct_answers = []
+        self.answers = []
+        
+        for k in list(self.bank.keys())[-1:0:-1]:
+            print(k)
+            self.questions += self.bank[k]['Question']
+            self.correct_answers += self.bank[k]['Correct Answer'] 
+            self.answers += self.bank[k]['answers']
+        
 
-        self.correct_answers = self.bank['1930 FIFA World Cup']['Correct Answer']
-
-        self.answers = self.bank['1930 FIFA World Cup']['answers']
-        print(self.answers)
-
+        self.edition = -1
         self.var = StringVar()
         self.var.set('0')
 
         self.question_number = 0
         self.score = 0
 
-        self.question = ttk.Label(self.frame_content, text = self.questions[self.question_number])
-        self.question.grid(row = 0, column = 0, padx = 20)
+        # Edition 
+        ttk.Label(self.frame_header, wraplength = 300,
+                text = (list(self.bank.keys())[-1])).grid(row = 1, column = 1)
 
+        # Add left and right padding to the frame
+        self.frame_content.grid_columnconfigure(0, weight = 1)
+        self.frame_content.grid_columnconfigure(1, weight = 1)
+        self.frame_content.grid_columnconfigure(2, weight = 1)
+
+
+        self.question = ttk.Label(self.frame_content, text = self.questions[self.question_number])
+        self.question.grid(row = 0, column = 0,  pady = 20)
         self.radio_one = ttk.Radiobutton(self.frame_content, text = self.answers[self.question_number][0],
                                         variable = self.var, value = self.answers[self.question_number][0])
         self.radio_one.grid(row = 1, column = 0, sticky = W, padx = 20)
@@ -68,44 +81,65 @@ class Quiz:
                                         variable = self.var, value = self.answers[self.question_number][3])
         self.radio_four.grid(row = 4, column = 0, sticky = W, padx = 20)
 
+
         self.button_frame = ttk.Frame(self.frame_content)
         self.button_frame.grid(row = 5, column = 0, pady = 10)
+        # Add left and right padding to the frame
+        self.button_frame.grid_columnconfigure(0, weight = 1)
+        self.button_frame.grid_columnconfigure(1, weight = 1)
+        self.button_frame.grid_columnconfigure(2, weight = 1)
+
 
         self.button_submit = ttk.Button(self.button_frame, text = "Submit", command = self.submit)
-        self.button_submit.grid(row = 0, column = 0, padx = 50)
+        self.button_submit.grid(row = 0, column = 0, padx = 20)
 
         self.button_next = ttk.Button(self.button_frame, text = "Next", command = self.next)
-        self.button_next.grid(row = 0, column = 1, padx = 50)
+        self.button_next.grid(row = 0, column = 1, padx = 20)
 
         self.button_quit = ttk.Button(self.button_frame, text = "Quit", command = self.quit)
-        self.button_quit.grid(row = 0, column = 2, padx = 50)
+        self.button_quit.grid(row = 0, column = 2, padx = 20)
+
+    
+        # Add score label
+        self.score_label = ttk.Label(self.frame_content, text = "Score: 0")
+        self.score_label.grid(row = 7, column = 0, pady = 20)
+
+        
+
+
+
+
+
 
     def quiz_bank(self):
+        # Questions
+        question_1,question_2,question_3,question_4 = "Quel est le pays organisateur ?","Quel est le pays vainqueur ?","Quel est le nombre de pays participants ?","Qui était le meilleur buteur ?"
+        questions = [question_1, question_2, question_3, question_4]
+    
+            
+        def fill_answers(df,val):
+            dict={}
+            col=['countryLabel','winnerLabel','participantsLabel','leaderLabel']
+            for col in col:
+                answers=df[df[col]!=val[col]][col].to_list()
+                unique_answers=list(set(answers))
+                alt_answers=random.sample(unique_answers,3)
+                alt_answers.append(val[col])
+                random.shuffle(alt_answers)
+                dict[col]=alt_answers
+                
+            return list(dict.values())
+
+    
         df = pd.read_csv('data.csv')
+        df['participantsLabel'] = df['participantsLabel'].astype(str)
         quiz_bank = {}
-        for ind,val in df.iterrows(): #['itemLabel.value'].unique():
-            df1 = df.drop([ind], axis=0)
-            r = [random.randint(0,len(df1)-1) for _ in range(3)] + [ind]
-            
-            question_1 = "Quel est le pays organisateur ?"
-            r_answer_1 = val['countryLabel']
-            answers_1 = df.reset_index().loc[r,'countryLabel'].tolist()
-            
-            question_2 = "Quel est le pays vainqueur ?"
-            r_answer_2 = val['winnerLabel']
-            answers_2 = df.reset_index().loc[r,'winnerLabel'].tolist()
-            
-            question_3 = "Quel est le nombre de pays participants ?"
-            r_answer_3 = val['participantsLabel']
-            answers_3 = df.reset_index().loc[r,'participantsLabel'].tolist()
-            
-            questions = [question_1, question_2, question_3]
-            answers = [r_answer_1,r_answer_2,r_answer_3]
-            alt_answers = [answers_1,answers_2,answers_3]
-            
+        for _,val in df.iterrows(): #['itemLabel.value'].unique():
+            r_answer_1,r_answer_2,r_answer_3,r_answer_4 = val['countryLabel'],val['winnerLabel'],val['participantsLabel'],val['leaderLabel']
+            answers = [r_answer_1,r_answer_2,r_answer_3,r_answer_4]
+            alt_answers=fill_answers(df,val)
             quiz_bank[val['itemLabel']] = {"Question": questions, "Correct Answer": answers, "answers": alt_answers}
         return quiz_bank
-
 
     def submit(self):
             
@@ -113,11 +147,13 @@ class Quiz:
     
             if self.var.get() == self.correct_answers[self.question_number]:
                 self.score += 1
+                self.score_label.config(text = "Score: {}".format(self.score))
                 messagebox.showinfo("Correct", "Your answer is correct!")
+                self.next()
             else:
-                messagebox.showinfo("Incorrect", "Your answer is incorrect!")
+                messagebox.showinfo("Incorrect", "Your answer is incorrect!, Retry....")
             
-            self.next()
+            
 
     def next(self):
                 
@@ -125,6 +161,11 @@ class Quiz:
         #print(self.var.get(),self.question_number)
         self.var.set(0)
         self.question_number += 1
+        # Update edition
+        if self.question_number %4 == 0:
+            self.edition -= 1
+            ttk.Label(self.frame_header, wraplength = 300,
+                text = (list(self.bank.keys())[self.edition])).grid(row = 1, column = 1)
 
         if self.question_number == len(self.questions):
             messagebox.showinfo("Quiz completed", "Your score is {}/{}".format(self.score, self.question_number))
